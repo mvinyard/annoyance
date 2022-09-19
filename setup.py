b255d3d8d9eb83b88fdbad071cf3daa19ae52d99
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2022-09-19 12:42:06 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 
 setuptools.setup(
     name="annoyance",
-    version="0.0.15",
+    version="0.0.16",
     python_requires=">3.7.0",
     author="Michael E. Vinyard - Harvard University - Massachussetts General Hospital - Broad Institute of MIT and Harvard",
     author_email="mvinyard@broadinstitute.org",
